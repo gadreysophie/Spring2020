2d735d87db401810879ad7b15d7dec2034371c5c
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-09-17 13:58:06 +0200

    Suppression méthode rdvsParProfessionnelEtDate : moved into RdvService.java

--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/dao/RdvDao.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/dao/RdvDao.java
@@ -10,11 +10,8 @@ import sample.data.jpa.domain.*;
 @Transactional
 public interface RdvDao extends JpaRepository<Rdv,Long> {
     @Query("Select r From Rdv r")
-    public List<Rdv> listRdvs();
+    List<Rdv> listRdvs();
 
     @Query("SELECT r FROM Rdv r WHERE r.id =:id")
-    public Rdv searchRdvById(@PathVariable("id") Long id);
-
-    @Query("SELECT r FROM Rdv r WHERE r.professionnel=:prof AND  r.dateDebut >=:date AND r.dateFin <=: date2")
-    public List<Rdv> rdvsParProfessionnelEtDate(@PathVariable("prof") Professionnel prof, @PathVariable("date") Date date);
+    Rdv searchRdvById(@PathVariable("id") Long id);
 }
