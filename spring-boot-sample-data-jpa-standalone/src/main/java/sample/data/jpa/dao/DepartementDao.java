d035f509d730a0d5819b297490de78fb4b36e54d
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-09-17 09:49:39 +0200

    Modif syntaxe to spring

--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/dao/DepartementDao.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/dao/DepartementDao.java
@@ -1,52 +1,13 @@
 package sample.data.jpa.dao;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.PathVariable;
 import sample.data.jpa.domain.Departement;
 
-public class DepartementDao {
+public interface DepartementDao extends JpaRepository<Departement, Long> {
 
-    private final EntityManager manager = EntityManagerHelper.getEntityManager();
+    @Query("SELECT d FROM Departement d WHERE d.id =:id")
+    public Departement searchDepartementById(@PathVariable("id") Long id);
 
-    public void createDepartements() {
-        int numOfDepartements = manager.createQuery("Select a From Departement a", Departement.class).getResultList().size();
-        if (numOfDepartements == 0) {
-            EntityTransaction tx = manager.getTransaction();
-            tx.begin();
-            manager.persist(new Departement("Master 1 - CCN"));
-            manager.persist(new Departement("Master 2 - CCNa"));
-            tx.commit();
-        }
-    }
-
-    public void listDepartements() {
-        List<Departement> resultList = manager.createQuery("Select a From Departement a", Departement.class).getResultList();
-        System.out.println("\nNombre de départements : " + resultList.size());
-        for (Departement next : resultList) {
-            System.out.println("Département suivant : " + next);
-        }
-        System.out.println();
-    }
-
-    public Departement searchDepartementById(Long id){
-        return (Departement) manager.createNamedQuery("departementParId").setParameter("id", id).getSingleResult();
-
-    }
-
-    public void addDepartement (Departement departement){
-        EntityTransaction tx = manager.getTransaction();
-        tx.begin();
-        manager.persist(departement);
-        tx.commit();
-    }
-
-    public void deleteDepartById (Long id){
-        EntityTransaction tx = manager.getTransaction();
-        tx.begin();
-        manager.remove(searchDepartementById(id));
-        tx.commit();
-    }
 }
