932495a758bbdd79dfa42dbae1f5e8b685b3074a
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-09-17 13:54:35 +0200

    Ajout type rdv resource

---
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/rest/TypeRdvResource.java
@@ -0,0 +1,49 @@
+package sample.data.jpa.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sample.data.jpa.dao.TypeRdvDao;
+import sample.data.jpa.domain.Professionnel;
+import sample.data.jpa.domain.TypeRdv;
+import java.util.List;
+
+@RestController("/typerdv")
+public class TypeRdvResource {
+    @Autowired
+    TypeRdvDao typeRdvDao;
+
+    @GetMapping(path="{/typeRdvId}",produces = "application/json")
+    public TypeRdv getTypeRdvById(@PathVariable("typeRdvId") Long typeRdvId)  {
+        return typeRdvDao.searchTypeRdvById(typeRdvId);
+    }
+
+    @GetMapping(path="/minTypeRdv{/prof}",produces = "application/json")
+    public Integer getMinDureeTypeRdvByProf(@PathVariable("prof") Professionnel professionnel)  {
+        return typeRdvDao.minDureeTypeRdvByProf(professionnel);
+    }
+
+    @GetMapping(path="/listTypeRdv{/prof}",produces = "application/json")
+    public List<TypeRdv> getListTypeRdvsParProf(@PathVariable("prof") Professionnel professionnel)  {
+        return typeRdvDao.listTypeRdvsParProf(professionnel);
+    }
+
+    @GetMapping(path="/listTypeRdv",produces = "application/json")
+    public List<TypeRdv> getTypeRdvs()  {
+        return typeRdvDao.listTypeRdvs();
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<TypeRdv> addTypeRdv(
+            @RequestBody TypeRdv typeRdv) {
+        typeRdvDao.save(typeRdv);
+        return ResponseEntity.ok(typeRdv);
+    }
+
+    @DeleteMapping(path="/{typeRdvId}")
+    public ResponseEntity<Void>  deleteTypeRdvById(@PathVariable("typeRdvId") Long typeRdvId)  {
+        typeRdvDao.delete(typeRdvDao.searchTypeRdvById(typeRdvId));
+        return ResponseEntity.accepted().build();
+    }
+}
+
