3caa10b61f41a508f1ba57f8beb969d02df7464e
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-09-22 08:39:23 +0200

    DepartementDao ajout listDepartements

--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/dao/DepartementDao.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/dao/DepartementDao.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import sample.data.jpa.domain.Departement;
+import java.util.List;
 
 public interface DepartementDao extends JpaRepository<Departement, Long> {
     @Query("SELECT d FROM Departement d WHERE d.id =:id")
     Departement searchDepartementById(@Param("id") Long id);
+
+    @Query("Select d From Departement d")
+    List<Departement> listDepartements() ;
 }
