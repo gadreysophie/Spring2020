0d348af8d0713707a989e90fd428ffea85ee181b
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-11-23 17:45:54 +0100

    changement : optimize import

--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/rest/UtilisateurResource.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/rest/UtilisateurResource.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sample.data.jpa.dao.UtilisateurDao;
-import sample.data.jpa.domain.Departement;
 import sample.data.jpa.domain.Utilisateur;
 
 import java.util.List;
