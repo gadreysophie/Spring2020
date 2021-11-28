7f12df3f0ebe5c32b5db1482f321e1901ce25275
Sophie Gadrey <gadreysophie@gmail.com>
2021-11-28 10:29:18 +0100

    add javadoc on the domain

--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/domain/Rdv.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/domain/Rdv.java
@@ -23,9 +23,19 @@ public class Rdv {
     private Date dateDebut;
     private Date dateFin;
 
+    /**
+     * To instantiate rdv
+     */
     public Rdv() {
     }
 
+    /**
+     * To create rdv on the database
+     * @param typeRdv the type of rdv
+     * @param professionnel the professional
+     * @param utilisateur the user
+     * @param dateDebut the beginning date
+     */
     public Rdv(TypeRdv typeRdv, Professionnel professionnel, Utilisateur utilisateur, Date dateDebut) {
         this.typeRdv = typeRdv;
         this.professionnel = professionnel;
@@ -38,6 +48,14 @@ public class Rdv {
         this.dateFin = c.getTime();
     }
 
+    /**
+     * To create rdv on the database
+     * @param id the rdv id
+     * @param typeRdv the type of rdv
+     * @param professionnel the professional
+     * @param utilisateur the user
+     * @param dateDebut the beginning date of the rdv
+     */
     public Rdv(Long id, TypeRdv typeRdv, Professionnel professionnel, Utilisateur utilisateur, Date dateDebut) {
         this.id = id;
         this.typeRdv = typeRdv;
@@ -51,59 +69,111 @@ public class Rdv {
         this.dateFin = c.getTime();
     }
 
+    /**
+     * To get the id of the rdv on the database
+     * @return the id
+     */
     @Id
     @GeneratedValue(generator="generatorIdRdv")
     public Long getId() {
         return id;
     }
 
+    /**
+     * To set the id
+     * @param id the id to set
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * To get the type of rdv of the rdv
+     * @return the type of rdv
+     */
     @ManyToOne
     public TypeRdv getTypeRdv() {
         return typeRdv;
     }
 
+    /**
+     * To set the type of rdv of the rdv
+     * @param typeRdv the type of rdv
+     */
     public void setTypeRdv(TypeRdv typeRdv) {
         this.typeRdv = typeRdv;
     }
 
+    /**
+     * To get the professional of the rdv
+     * @return the professional
+     */
     @ManyToOne
     public Professionnel getProfessionnel() {
         return professionnel;
     }
 
+    /**
+     * To set the professional of the rdv
+     * @param professionnel the professional
+     */
     public void setProfessionnel(Professionnel professionnel) {
         this.professionnel = professionnel;
     }
 
+    /**
+     * To get the user of the rdv
+     * @return the user
+     */
     @ManyToOne
     public Utilisateur getUtilisateur() {
         return utilisateur;
     }
 
+    /**
+     * To set the user of the rdv
+     * @param utilisateur the user
+     */
     public void setUtilisateur(Utilisateur utilisateur) {
         this.utilisateur = utilisateur;
     }
 
+    /**
+     * To get the beginning date of the rdv
+     * @return the beginning date
+     */
     public Date getDateDebut() {
         return dateDebut;
     }
 
+    /**
+     * To set the beginning date of the rdv
+     * @param dateDebut the beginning date
+     */
     public void setDateDebut(Date dateDebut) {
         this.dateDebut = dateDebut;
     }
 
+    /**
+     * To get the end date of the rdv
+     * @return the end date of the rdv
+     */
     public Date getDateFin() {
         return dateFin;
     }
 
+    /**
+     * To set the end date of the rdv
+     * @param dateFin the end date
+     */
     public void setDateFin(Date dateFin) {
         this.dateFin = dateFin;
     }
 
+    /**
+     * To display data on the rdv
+     * @return a String of data on the rdv
+     */
     @Override
     public String toString() {
         return "RDV [id=" + id + ", Type de RDV=" + typeRdv + ", professionnel=" + professionnel + ", " +
--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/domain/Utilisateur.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/domain/Utilisateur.java
@@ -18,31 +18,67 @@ public class Utilisateur extends Personne {
 
     private List<Rdv> rdvs = new ArrayList<>();
 
+    /**
+     * To instantiate user
+     */
     public Utilisateur() {
     }
 
+    /**
+     * To create user on the database
+     * @param nom the name of the user
+     */
     public Utilisateur (String nom) {
         super(nom);
     }
 
+    /**
+     * To create user on the database
+     * @param nom the name of the user
+     * @param prenom the lastname of the user
+     * @param identifiant the login of the user
+     * @param mail the email of the user
+     * @param mdp the password of the user
+     */
     public Utilisateur (String nom, String prenom, String identifiant, String mail, String mdp) {
         super(nom, prenom, identifiant, mail, mdp);
     }
 
+    /**
+     * To create the user on the database
+     * @param id the id of the user
+     * @param nom the name of the user
+     * @param prenom the lastname of the user
+     * @param identifiant the login of the user
+     * @param mail the email of the user
+     * @param mdp the password of the user
+     */
     public Utilisateur (Long id, String nom, String prenom, String identifiant, String mail, String mdp) {
         super(id, nom, prenom, identifiant, mail, mdp);
     }
 
+    /**
+     * To get the list of rdv of the user
+     * @return a list of rdv
+     */
     @OneToMany(mappedBy = "utilisateur", cascade = CascadeType.PERSIST)
     @JsonIgnore
     public List<Rdv> getRdvs() {
         return rdvs;
     }
 
+    /**
+     * To set rdv for the user
+     * @param rdvs a list of rdv
+     */
     public void setRdvs(List<Rdv> rdvs){
         this.rdvs = rdvs;
     }
 
+    /**
+     * To display data on the user
+     * @return a String of data on the user
+     */
     @Override
     public String toString() {
         return "Utilisateur [id=" + getId() + ", nom=" + getNom() + ", prénom=" + getPrenom() + "]";
